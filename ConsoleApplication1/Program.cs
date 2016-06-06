fc0f27cb634f253cc7177d30c27737d73fc57c04
ZI YANG Li <t_liz@ads.autodesk.com>
2016-06-06 16:51:27 +0800

    crap, the xml we applied auto generate xsd to is not complex enough

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -14,31 +14,126 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             var jsonString = System.IO.File.ReadAllText(@"C:\Users\t_liz\documents\visual studio 2015\Projects\ConsoleApplication2\ConsoleApplication1\TextFile1.json");
-            var y = deserializeToDictionary(jsonString);
-            pXMLTable x = new pXMLTable();
+            var o = JObject.Parse(jsonString);
+            transformToPXmlTable(o);
+            
             var z = pXMLTable.LoadFromFile(@"C:\Users\t_liz\Documents\visual studio 2015\Projects\WindowsFormsApplication4\WindowsFormsApplication4\XMLFile1.xml");
             
-            z.SaveToFile(@"C:\Users\t_liz\Documents\visual studio 2015\Projects\WindowsFormsApplication4\WindowsFormsApplication4\wtf.xml");
+            //z.SaveToFile(@"C:\Users\t_liz\Documents\visual studio 2015\Projects\WindowsFormsApplication4\WindowsFormsApplication4\wtf.xml");
             var t = 1 + 1;
         }
 
-        private static Dictionary<string, object> deserializeToDictionary(string jo)
+
+        private static pXMLTable transformToPXmlTable(JObject root)
         {
-            var values = JsonConvert.DeserializeObject<Dictionary<string, object>>(jo);
-            var values2 = new Dictionary<string, object>();
-            foreach (KeyValuePair<string, object> d in values)
+            pXMLTable p = new pXMLTable();
+
+            // pXMLTable attributes
+            p.desc      = "Part Table";
+            p.fixColumn = "C1";
+            p.fileMigrate = "False"; // may not exist for every xml
+            p.version   = 1;
+
+            // ColumnConstView
+            p.ColumnConstView           = new pXMLTableColumnConstView();
+            p.ColumnConstView.desc      = "Parameter-driven Display";
+            p.ColumnConstView.id        = "CCV1";
+            p.ColumnConstView.viewKey   = "3d";
+            p.ColumnConstView.viewName  = "AecbPartRecipe";
+            
+            p.ColumnConstView.Images = new pXMLTableColumnConstViewImages();
+            p.ColumnConstView.Images.Image = new pXMLTableColumnConstViewImagesImage();
+            p.ColumnConstView.Images.Image.URL = new pXMLTableColumnConstViewImagesImageURL();
+            p.ColumnConstView.Images.Image.URL.title = "Part Reference Image";
+            p.ColumnConstView.Images.Image.URL.href = root.SelectToken("additionalInfo").SelectToken("bmpPath").ToString();
+
+            p.ColumnConstView.Recipe = root.SelectToken("additionalInfo").SelectToken("dwgPath").ToString();
+
+            // ColumnUnique
+            p.ColumnUnique = new pXMLTableColumnUnique();
+            p.ColumnUnique.desc = "Primary Key";
+            p.ColumnUnique.datatype = "string";
+            p.ColumnUnique.name = "UUID";
+            p.ColumnUnique.visible = 0;
+            
+            p.ColumnUnique.RowUnique = new List<pXMLTableColumnUniqueRowUnique>();
+            JArray columnUniques = (JArray)root.SelectToken("additionalInfo").SelectToken("ColumnUnique");
+            for (int i = 0; i < columnUniques.Count; i++)
             {
-                // if (d.Value.GetType().FullName.Contains("Newtonsoft.Json.Linq.JObject"))
-                if (d.Value is JObject)
+                var ru = new pXMLTableColumnUniqueRowUnique();
+                ru.Value = columnUniques[i].ToString();
+                ru.id = "r" + i;
+                p.ColumnUnique.RowUnique.Add(ru);
+            }
+
+            // Column
+            p.Column = new List<pXMLTableColumn>();
+            IEnumerable<KeyValuePair<string, JToken>> columns = (JObject)root.SelectToken("basicTable");    // downcast from JToken to JObject
+            foreach (KeyValuePair<string, JToken> kvp in columns)
+            {
+                var column = new pXMLTableColumn();
+                // "decompress" attribute with assumed fixed order
+                string[] attrSeq = ((JArray) kvp.Value.SelectToken("attr")).ToObject<string[]>();
+                column.dataType = attrSeq[0];
+                column.unit     = attrSeq[1];
+                column.name     = attrSeq[2];
+                column.id       = attrSeq[3];
+                column.visible  = Convert.ToByte(Int32.Parse(attrSeq[4]));
+                column.context  = attrSeq[5];
+                column.index    = Convert.ToByte(Int32.Parse(attrSeq[6]));
+                column.desc     = kvp.Key;                                      // remember in xml-to-json transformation, columns are keyed by "desc"
+
+                // reconstruct rows
+                string[] entries = ((JArray) kvp.Value.SelectToken("entries")).ToObject<string[]>();
+                for (int i = 0; i < entries.Length; i++)
                 {
-                    values2.Add(d.Key, deserializeToDictionary(d.Value.ToString()));
+                    var row = new pXMLTableColumnRow();
+                    row.id = "r" + i;
+                    row.Value = Convert.ToDecimal(entries[i]);
+                    column.Row.Add(row);
                 }
-                else
+
+                p.Column.Add(column);
+            }
+
+
+            // ColumnConstList
+            p.ColumnConstList = new List<pXMLTableColumnConstList>();
+            IEnumerable<KeyValuePair<string, JToken>> columnConstLists = (JObject)root.SelectToken("constantLists");
+            foreach (KeyValuePair<string, JToken> kvp in columnConstLists)
+            {
+                var columnConstList = new pXMLTableColumnConstList();
+                // "decompress" attribute
+                string[] attrSeq = ((JArray)kvp.Value.SelectToken("attr")).ToObject<string[]>();
+                columnConstList.dataType = attrSeq[0];
+                columnConstList.unit = attrSeq[1];
+                columnConstList.name = attrSeq[2];
+                columnConstList.id = attrSeq[3];
+                columnConstList.visible = Convert.ToByte(Int32.Parse(attrSeq[4]));
+                columnConstList.context = attrSeq[5];
+                columnConstList.index = Convert.ToByte(Int32.Parse(attrSeq[6]));
+                columnConstList.desc = kvp.Key;
+
+                // reconstruct rows
+                string[] entries = ((JArray)kvp.Value.SelectToken("entries")).ToObject<string[]>();
+                for (int i = 0; i < entries.Length; i++)
                 {
-                    values2.Add(d.Key, d.Value);
+                    var row = new pXMLTableColumnRow();
+                    row.id = "r" + i;
+                    //row.Value = Convert.ToDecimal(entries[i]);
+                    //columnConstList.Item.
                 }
             }
-            return values2;
+
+
+            // ColumnConst
+
+
+            // ColumnCalc
+
+
+
+            return p;
         }
     }
 }
